5e14833b7a9642b4078a0fc6ba32233c4ef27d62
Roman Sechenov <kpdprof@gmail.com>
2023-03-03 02:47:18 +0700

    new version

--- Learning_Java/src/lesson_1/Main.java
+++ Learning_Java/src/lesson_1/Main.java
@@ -1,5 +1,7 @@
 package lesson_1;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         var varByte = 1; // 8 bit / -128..127
@@ -15,40 +17,28 @@ public class Main {
 
         System.out.println(Integer.MAX_VALUE);
         // Числа по умолчанию равны 0, для логических – false, для ссылок на объект – null
-        System.out.println(drawIsMonitorsCounter(21, 10));
+        System.out.println(drawIsMonitorsCounter(9, 10));
+        System.out.println(priceCalculation(8.50, 2));
+
+        String string = new String("Hello");
+        System.out.println(string);
+
+        Scanner scanner = new Scanner(System.in);
+
     }
 
     // Сигнатура метода — это имя метода плюс параметры (причем порядок параметров имеет значение).
     // В сигнатуру метода не входит возвращаемое значение, а также бросаемые им исключения.
     public static int drawIsMonitorsCounter(int monitors, int programmers) {
-        if (monitors == programmers) {
-            return 0;
-        } else if (monitors > programmers) {
-            return monitors % programmers;
-        } else if (monitors < programmers) {
-            return 0;
-        } else {
-            return 0;
-        }
+        return monitors % programmers;
     }
 
-    public static void priceCalculation(double price, int count) {
-
+    public static double priceCalculation(double price, int count) {
+        return price * count;
     }
 
 }
 
-
-
-
-
-
 // Как создать пакет через консоль
 // Загуглив пакеты util java, не вижу Scanner, где он лежит
 // где в IDE лежит util и как его найти
-
-// где находятся методы оберток (в какой пакете) и чем статические константы оберток и где они обитают
-// valueOf, parseInt, MAX_VALUE, MIN_VALUE
-
-//Какие основыне источники решения задач на работе?
-//Часто используются книги для решения задач или в основном гугл, документация, документация компании
